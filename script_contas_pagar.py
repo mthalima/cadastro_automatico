eed5144e5301ac70155f4b6752cd529b7f1b9ced
Matheus <matheus.ecomp@gmail.com>
2023-05-25 17:27:48 -0300

    add detecção automatica da situação do motorista

--- script_contas_pagar.py
+++ script_contas_pagar.py
@@ -4,67 +4,72 @@ from time import sleep
 
 df = pd.read_excel('lista_pagamento.xlsx')
 
-#print(df)
+sleep(1)
 
-sleep(2)
-
-s1 = sleep(0.5)
-s2= sleep(0.3)
-
-def cad_doc():
+def cad_doc():  
+    pyg.click(704,77) #click em incluir
+    sleep(0.3)
     pyg.click(226,182, duration=1)
-    s1
     pyg.press('f2')
     pyg.write(str(i.nome))
-    s1
+    sleep(0.3)
     pyg.press('f4')
-    pyg.doubleClick(620,428, duration=1)
-    s1
-    pyg.press('tab')
-    pyg.write('0')
-    s1
-    pyg.press('tab')
-    pyg.write(str(i.documento))
-    s1
-    pyg.press('tab')
-    pyg.write('SAL')
-    s1
-    pyg.press('tab')
-    pyg.write('1')
-    s1
-    pyg.press('tab')
-    pyg.write('1')
-    s1
-    pyg.press('tab')
-    s1
-    pyg.press('tab')
-    pyg.write('237')
-    s1
-    pyg.press('tab')
-    pyg.press('tab')
-    pyg.write('230520230800')
-    s1
-    pyg.press('tab')
-    pyg.write('230520230800')
-    s1
-    pyg.press('tab')
-    pyg.write(str(i.salario))
-    s1
-    pyg.click(408,525, duration=0.5)
-    pyg.write('PAGAMENTO SALARIO 04/2023')
-    pyg.click(755,79, duration= 1)
+    sleep(1)
     
-    cad_parc()
-    cad_parc()
-    
-    pyg.press('enter')
-    
+    if check_situac() != None:
+        pyg.doubleClick(620,428, duration=1)
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write('0')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write(str(i.documento))
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write('SAL')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write('1')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write('1')
+        sleep(0.3)
+        pyg.press('tab')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write('237')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.press('tab')
+        pyg.write('250520230800')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write('250520230800')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write(str(i.salario))
+        sleep(0.3)
+        pyg.click(408,525, duration=0.5)
+        pyg.write('PAGAMENTO SALARIO 04/2023')
+        sleep(0.3)
+        pyg.press('tab')
+        pyg.write('24')
+        pyg.click(755,79, duration= 1) # click em salvar
+        sleep(0.3)
+        cad_parc()
+    else:
+        log_erro()
+        pyg.click(1046,181, duration=0.5)
+        
+        
 def cad_parc():
     pyg.click(111,157, duration=0.5)
-    s1
+    sleep(0.3)
     pyg.click(22,183, duration=0.5)
+    sleep(0.3)
     pyg.click(851,188, duration=0.5)
-    
+    sleep(0.3)
+
 def pag_clas():
     pyg.click(29,205, duration=0.5)
     pyg.write('3')
@@ -84,6 +89,15 @@ def pag_clas():
     pyg.press('tab')
     pyg.write(str(i.salario))
 
+def check_situac():
+    situac = pyg.locateOnScreen('func_ativo.png', region=(294,398, 350,440))
+    return situac
+
+def log_erro():
+    with open("log_erro.txt", "a") as arquivo:
+        arquivo.write(i.nome)
+        arquivo.write('\n')
  
+
 for i in df.itertuples(): 
-    cad_doc()
+    documento = cad_doc()
